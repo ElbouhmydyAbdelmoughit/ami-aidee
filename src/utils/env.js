73727671ca872452e248e5a4bde59e18589218f9
Dang Hai An <an@izberg-marketplace.com>
2020-01-05 23:00:23 +0100

    fix: video bug on certain android tablets
    Android 7.0 or less has a bug on ssl handshake:
    https://stackoverflow.com/questions/39133437/sslhandshakeexception-handshake-failed-on-android-n-7-0
    
    So we need to serve media API for BOTH http and https protocols, and decide which protocol to use based on ANDROID version (<= 25: 7.0 or less)

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,5 +1,19 @@
+import { Platform } from "react-native"
+
+const HTTP_MEDIA_URL = "http://d2ppax42vvtce1.cloudfront.net"
+const HTTPS_MEDIA_URL = "https://d2ppax42vvtce1.cloudfront.net"
+
+const getMediaUrl = () => {
+  if (Platform.OS === "android" && Platform.Version <= 25) {
+    console.log("using http url", Platform.OS, Platform.Version)
+    return HTTP_MEDIA_URL
+  }
+  console.log("using https url", Platform.OS, Platform.Version)
+  return HTTPS_MEDIA_URL
+}
+
 export const Env = {
   API_URL: "http://15.188.62.36:3333",
-  GRAPHQL_URL: "http://15.188.62.36:8080/v1/graphql", 
-  MEDIA_URL: "https://bequille-ami-dev.s3.eu-west-3.amazonaws.com"
+  GRAPHQL_URL: "http://15.188.62.36:8080/v1/graphql",
+  MEDIA_URL: getMediaUrl(),
 }
