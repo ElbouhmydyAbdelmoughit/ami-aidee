dc02221f9753f1d6a4ec52a5d67e3024cfc33697
Dang Hai An <danghaian168@gmail.com>
2021-01-23 22:45:23 +0100

    fix min_volume manager

--- src/ui/business/SystemVolumeManager/SystemVolumeManager.tsx
+++ src/ui/business/SystemVolumeManager/SystemVolumeManager.tsx
@@ -19,10 +19,12 @@ import AppStyles from 'config/styles'
 import { getGradientColors } from 'utils/colors'
 import { moments, times } from 'utils'
 import moment from 'moment'
+import useDebounce from 'ui/hooks/use-debounce'
 
 const SystemVolumeManager = () => {
   const helpedUser = useSelector(AuthSelectors.getCurrentHelpedUser)
   const [sufficientVolume, setSufficientVolume] = useState(true)
+  const sufficientVolumeDebounced = useDebounce(sufficientVolume, 3000)
   const opacityAnim = useRef(new Animated.Value(0)).current
 
   const loop = Animated.loop(
@@ -47,35 +49,55 @@ const SystemVolumeManager = () => {
       setTimeout(() => {
         Setting.setVolume(helpedUser.min_volume)
         setProgress(helpedUser.min_volume)
-      }, 1000)
-      setTimeout(() => {
         setSufficientVolume(true)
-      }, 2000)
+      }, 3000)
     } else {
       loop.stop()
     }
   }, [sufficientVolume])
+
   const appState = useAppState()
 
+  const onNewVolume = (volume: number) => {
+    console.log('newVolume', volume)
+    if (!helpedUser.min_volume) {
+      return
+    }
+    setProgress(volume)
+    if (volume < helpedUser.min_volume - 0.1) {
+      setSufficientVolume(false)
+    } else {
+      setSufficientVolume(true)
+    }
+  }
   useEffect(() => {
-    console.log(appState)
     Setting.getVolume().then(volume => {
-      setProgress(volume)
-      if (volume < helpedUser.min_volume - 0.1) {
-        setSufficientVolume(false)
-      } else {
-        setSufficientVolume(true)
-      }
+      onNewVolume(volume)
     })
   }, [helpedUser.min_volume, appState])
 
+  useEffect(() => {
+    const listener = Setting.addVolumeListener(volumeData => {
+      onNewVolume(volumeData.value)
+    })
+
+    return () => {
+      Setting.removeVolumeListener(listener)
+    }
+  }, [helpedUser.min_volume])
+
   const time = times(moment(), helpedUser)
-  const color = getGradientColors(time)[0]
+  const color = sufficientVolume
+    ? getGradientColors(time)[0]
+    : AppStyles.colors.danger
   const window = useDimensions()
-  console.log('progress', progress, color)
 
   return (
-    <Modal transparent visible={!sufficientVolume} pointerEvents="none">
+    <Modal
+      transparent
+      visible={!sufficientVolume || !sufficientVolumeDebounced}
+      pointerEvents="none"
+    >
       <View style={modalStyles.modalBackground}>
         <View
           style={{
--- src/ui/screens/userSettings/UserSettingsScreen.tsx
+++ src/ui/screens/userSettings/UserSettingsScreen.tsx
@@ -3,7 +3,7 @@ import GradientBackground from 'ui/components/GradientBackground'
 import { Actions } from 'react-native-router-flux'
 import { Checkbox, Button, TouchableRipple } from 'react-native-paper'
 import { H1 } from 'native-base'
-import { View, StyleSheet, Text, TouchableHighlight } from 'react-native'
+import { View, StyleSheet, Text, ScrollView } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import useActivityLog from 'ui/hooks/use-activity-log'
 import { HelpedUser, TrackedActivity } from 'core/types'
@@ -77,11 +77,10 @@ const UserSettingsScreen = ({
 
   return (
     <GradientBackground>
-      <View
-        style={{
+      <ScrollView
+        style={{ flex: 1 }}
+        contentContainerStyle={{
           marginLeft: 64,
-          alignItems: 'flex-start',
-          height: '100%',
         }}
       >
         <View
@@ -92,96 +91,98 @@ const UserSettingsScreen = ({
         >
           <H1 style={styles.title}>Paramètres</H1>
         </View>
-        <View style={{ flexDirection: 'row' }}>
-          <TouchableRipple
-            mode="outlined"
-            dark
-            style={{
-              borderColor: 'white',
-              borderRadius: 4,
-              borderWidth: 1,
-              marginBottom: 24,
-            }}
-            onPress={() => {
-              logActivity(TrackedActivity.RETURN_FROM_USER_SETTINGS)
-              Actions.pop()
-            }}
-          >
-            <View
-              style={{
-                alignItems: 'baseline',
-                flexDirection: 'row',
-                padding: 16,
-                paddingTop: 8,
-                paddingBottom: 8,
-              }}
-            >
-              <Icon name="keyboard-return" size={16} color={'white'} />
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 18,
-                  textTransform: 'uppercase',
-                  fontWeight: '700',
-                  marginLeft: 8,
-                }}
-              >
-                Revenir
-              </Text>
-            </View>
-          </TouchableRipple>
-        </View>
         <View style={{ flex: 1 }}>
           <View style={{ flexDirection: 'row' }}>
-            <Checkbox
-              color="white"
-              status={checked ? 'checked' : 'unchecked'}
-              onPress={onPress}
-              uncheckedColor="white"
-            />
-            <TouchableRipple onPress={onPress}>
-              <React.Fragment>
-                <Text style={styles.text}>Décrochage automatique</Text>
-                <Text style={styles.helpText}>
-                  L'appel entrant sera décroché automatiquement après 3
-                  secondes.
+            <TouchableRipple
+              mode="outlined"
+              dark
+              style={{
+                borderColor: 'white',
+                borderRadius: 4,
+                borderWidth: 1,
+                marginBottom: 24,
+              }}
+              onPress={() => {
+                logActivity(TrackedActivity.RETURN_FROM_USER_SETTINGS)
+                Actions.pop()
+              }}
+            >
+              <View
+                style={{
+                  alignItems: 'baseline',
+                  flexDirection: 'row',
+                  padding: 16,
+                  paddingTop: 8,
+                  paddingBottom: 8,
+                }}
+              >
+                <Icon name="keyboard-return" size={16} color={'white'} />
+                <Text
+                  style={{
+                    color: 'white',
+                    fontSize: 18,
+                    textTransform: 'uppercase',
+                    fontWeight: '700',
+                    marginLeft: 8,
+                  }}
+                >
+                  Revenir
                 </Text>
-              </React.Fragment>
+              </View>
             </TouchableRipple>
           </View>
-          <View style={{ flexDirection: 'row', marginTop: 24 }}>
-            <Checkbox
-              color="white"
-              status={minVolumeChecked ? 'checked' : 'unchecked'}
-              onPress={onMinVolumePress}
-              uncheckedColor="white"
-            />
-            <TouchableRipple onPress={onMinVolumePress}>
-              <React.Fragment>
-                <Text style={styles.text}>Volume activé</Text>
-                <Text style={styles.helpText}>
-                  Le volume sera toujours activé et ne descend pas au dessous de
-                  50%
-                </Text>
-              </React.Fragment>
-            </TouchableRipple>
-          </View>
-          <View style={{ flexDirection: 'row', marginTop: 24, flex: 1 }}>
-            <Checkbox
-              color="white"
-              status={dischargingAlertChecked ? 'checked' : 'unchecked'}
-              onPress={onAlertOnDischargePress}
-              uncheckedColor="white"
-            />
-            <TouchableRipple onPress={onAlertOnDischargePress}>
-              <React.Fragment>
-                <Text style={styles.text}>Alerte débranchement</Text>
-                <Text style={styles.helpText}>
-                  Lorsque la tablette est débranché, l'aidé sera alerté pour
-                  rebrancher sa tablette
-                </Text>
-              </React.Fragment>
-            </TouchableRipple>
+          <View style={{ flex: 1 }}>
+            <View style={{ flexDirection: 'row' }}>
+              <Checkbox
+                color="white"
+                status={checked ? 'checked' : 'unchecked'}
+                onPress={onPress}
+                uncheckedColor="white"
+              />
+              <TouchableRipple onPress={onPress}>
+                <React.Fragment>
+                  <Text style={styles.text}>Décrochage automatique</Text>
+                  <Text style={styles.helpText}>
+                    L'appel entrant sera décroché automatiquement après 3
+                    secondes.
+                  </Text>
+                </React.Fragment>
+              </TouchableRipple>
+            </View>
+            <View style={{ flexDirection: 'row', marginTop: 24 }}>
+              <Checkbox
+                color="white"
+                status={minVolumeChecked ? 'checked' : 'unchecked'}
+                onPress={onMinVolumePress}
+                uncheckedColor="white"
+              />
+              <TouchableRipple onPress={onMinVolumePress}>
+                <React.Fragment>
+                  <Text style={styles.text}>Volume activé</Text>
+                  <Text style={styles.helpText}>
+                    Le volume sera toujours activé et ne descend pas au dessous
+                    de 50%
+                  </Text>
+                </React.Fragment>
+              </TouchableRipple>
+            </View>
+            <View style={{ flexDirection: 'row', marginTop: 24, flex: 1 }}>
+              <Checkbox
+                color="white"
+                status={dischargingAlertChecked ? 'checked' : 'unchecked'}
+                onPress={onAlertOnDischargePress}
+                uncheckedColor="white"
+              />
+              <TouchableRipple onPress={onAlertOnDischargePress}>
+                <React.Fragment>
+                  <Text style={styles.text}>Alerte débranchement</Text>
+                  <Text style={styles.helpText}>
+                    Lorsque la tablette est débranché, l'aidé sera alerté pour
+                    rebrancher sa tablette
+                  </Text>
+                </React.Fragment>
+              </TouchableRipple>
+            </View>
           </View>
           <View
             style={{ alignItems: 'flex-start', justifyContent: 'flex-end' }}
@@ -225,7 +226,7 @@ const UserSettingsScreen = ({
             </TouchableRipple>
           </View>
         </View>
-      </View>
+      </ScrollView>
     </GradientBackground>
   )
 }
