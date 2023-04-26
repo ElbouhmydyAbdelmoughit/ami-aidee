cc1da1daba0f12b121ac578f0a52a4ea4bafa40e
Fernand Lime <fernand.lime@gmail.com>
2023-04-26 12:08:54 +0200

    prettier

--- native-base-theme/components/TabContainer.js
+++ native-base-theme/components/TabContainer.js
@@ -1,12 +1,12 @@
 // @flow
 
-import { Platform } from 'react-native';
+import { Platform } from 'react-native'
 
-import variable from './../variables/platform';
-import { PLATFORM } from './../variables/commonColor';
+import variable from './../variables/platform'
+import { PLATFORM } from './../variables/commonColor'
 
 export default (variables /* : * */ = variable) => {
-  const platformStyle = variables.platformStyle;
+  const platformStyle = variables.platformStyle
 
   const tabContainerTheme = {
     elevation: 3,
@@ -19,8 +19,8 @@ export default (variables /* : * */ = variable) => {
     shadowRadius: platformStyle === PLATFORM.MATERIAL ? 1.2 : undefined,
     justifyContent: 'space-around',
     borderBottomWidth: Platform.OS === PLATFORM.IOS ? variables.borderWidth : 0,
-    borderColor: variables.topTabBarBorderColor
-  };
+    borderColor: variables.topTabBarBorderColor,
+  }
 
-  return tabContainerTheme;
-};
+  return tabContainerTheme
+}
